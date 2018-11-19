96fed1345ccfea19a3e12ea60042200097e57a9c
AbdullahTaher93 <bookaa641@gmail.com>
2018-11-19 07:21:13 +0300

    test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,38 @@
-var http=require('http');
-var m=http.createServer(function(req,res){
-    res.writeHead(200);
-    res.write('hola todo');
-    res.end();
-});
-m.listen(8080);
-console.log('we listen to http://127.0.0.1:8080');
+var request = require('supertest');
+var app = require('../IssueBot');
+var IssueManager = require('../IssueManager');
+const assert = require('assert');
 
 
+//==================== Server tests ====================
 
-
+describe('Server test', function() {
 	
+	it('responds {"status":"OK"} to /', function (done){
+		request(app)
+		.get('/')
+		.set('Accept', 'application/json')
+		.expect('Content-Type',/json/)
+		.expect(200)
+		.end(function(err,result){
+			assert.equal(result.body.status, "OK");
+		});
+		done();
+		
+	});
+
+	it('responds with the issues to /see_issues', function(done){
+		request(app)
+		.get('/see_issues')
+		.set('Accept', 'application/json')
+		.expect('Content-Type',/json/)
+		.expect(200)
+		.end(function(err,result){
+			assert.equal(result.body.size, 0);
+		});
+		done();
+
+	});
+
+
+});
