257ea47bd326d47d5c359a6e22a243c3e7cdbdb8
AbdullahTaher93 <bookaa641@gmail.com>
2018-11-19 07:10:37 +0300

    test

--- test/test.js
+++ test/test.js
@@ -1,24 +1,13 @@
-var express=require("express");
-	var app = express();
-    var port = process.env.PORT || 8080;
-    var p=
-    '<html><head><title>My project in Heroku</title></head>'+
-    '<body>'+
-    '<form method="post" action="/"><br>'+
-    'first name:<input name="firstname"><br>'+
-    '</form>'
-    '</body>'+
-    '</html>';
+var http=require('http');
+var m=http.createServer(function(req,res){
+    res.writeHead(200);
+    res.write('hola todo');
+    res.end();
+});
+m.listen(8080);
+console.log('we listen to http://127.0.0.1:8080');
 
-	app.get("/", function (req, res) {
-        res.send( {"status":"OK","ejemplo":{"ruta":"/see_issues","valor":{"size":0}}} );
-        
-        
-	});
 
-	app.get("/proc", function (req, res) {
-		res.send( { Portada: true } );
-	});  
 
-	app.listen(port);
-	console.log("Server running at http://127.0.0.1:"+port+"/");
+
+	
