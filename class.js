cd3551f1e5f60689b3abf2e2f28077dc964e000d
AbdullahTaher93 <abdullahtaher@correo.ugr.es>
2019-08-22 04:42:35 +0200

    edit procfile

--- class.js
+++ class.js
@@ -1,6 +1,6 @@
 //using mongodb
 var MongoClient=require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://localhost:27017/persons";
 
 
 
@@ -21,8 +21,8 @@ var data = {
     email:'',
     phonenumber:0,
     sex:"",
-
-
+  
+  
     setname: function(name) {
       this.name=name;
     },
@@ -37,9 +37,6 @@ var data = {
     },
     setskills: function(skills) {
       this.skills=skills;
-    },
-    getskills: function(){
-      return skills;
     },
     getskills: function(){
       return skills;
@@ -74,9 +71,9 @@ function storeData(name,age,sex,email,phonenumber,skills,study){
     let clone = {};
     for (let key in data) {
       clone[key] = data[key];
-
+      
     }
-
+  
     clone.setname(name);
     clone.setage(age);
     clone.setsex(sex);
@@ -84,9 +81,9 @@ function storeData(name,age,sex,email,phonenumber,skills,study){
     clone.setphone(phonenumber);
     clone.setskills(skills);
     clone.setstudy(study);
-
-
-
+    
+    
+    
 
 
     savedata.push(clone);
@@ -95,11 +92,7 @@ function storeData(name,age,sex,email,phonenumber,skills,study){
       if (err) throw err;
       
       var dbase = db.db("persons"); //here
-      var json;
-      
-        
-        
-           json={
+      var json={
             ID:size,
             name:name,
             age:age,
@@ -108,18 +101,16 @@ function storeData(name,age,sex,email,phonenumber,skills,study){
             phonenumber:phonenumber,
             skills:skills,
             study:study
-            
-
-          };
-     
+                };
+      console.log("db has created");
       dbase.collection("profiles").insertOne(json,function(err, res){
           if (err) throw err;
-          console.log("profile created!");
+          console.log("profile created!"+json.name);
           db.close();   //close method has also been moved to client obj
       });
   });
     size=size+1;
-
+     
   }
   //edit a profile
   function editdata(index,name,age,sex,email,phonenumber,skills,study){
@@ -133,22 +124,22 @@ function storeData(name,age,sex,email,phonenumber,skills,study){
   }
 //delete a profile
 function deletdata(index){
-
-  MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {   //here db is the client obj
+  
+  /*MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {   //here db is the client obj
     if (err) throw err;
-
+    
     var dbase = db.db("persons"); //here
-
-
+    
+    
       var query={ID:index};
-
-
+      
+        
     dbase.collection("profiles").deleteOne(query,function(err, res){
         if (err) throw err;
         console.log("profile deleted!");
         db.close();   //close method has also been moved to client obj
     });
-});
+});*/
 
 
 var items=savedata.splice(index,1);
@@ -165,6 +156,7 @@ var si=0;
   si=si+1;
   }
   return namesfilter;
+  
 });
 
 
@@ -176,37 +168,42 @@ function search1(age){
 
   MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {   //here db is the client obj
     if (err) throw err;
-
+ 
     var dbase = db.db("persons"); //here
-
-
+    
+    
       var query={age:age};
-
-
+      
+        
     dbase.collection("profiles").find().sort(query).toArray(function(err, res){
         if (err) throw err;
         console.log(" searching!");
         db.close();   //close method has also been moved to client obj
     });
 });
-
-
-
+  
+  
+  
   }
-
+  
 
 //print all fprofiles in Console.....
   function print(){
-
+   
     savedata.forEach(function(item, index) {
       console.log('['+(index+1)+'] '+item.name+' '+item.age+'  '+item.sex+'  '+item.email+'  '+item.phonenumber+'  '+item.skills+'  '+item.study);
-
+      
       jsonreturn.push({"ID":(index+1),"name":item.name,"Age":item.age,"sex":item.sex,"email":item.email,"phonenumber":item.phonenumber,"skills":item.skills,"Study":item.study});
     }
-
+    
     );
+  
+
   }
 
 
 
   module.exports={storeData,savedata,print,editdata,deletdata,search,namesfilter,size,jsonreturn};
+
+
+
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //using mongodb
 var express=require("express");
 var MongoClient=require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://localhost:27017/persons";
 MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {   //here db is the client obj
     if (err) throw err;
     console.log("db has created");
@@ -12,35 +12,23 @@ MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {   //here 
         db.close();   //closed method has also been moved to client obj
     
 });
-});
-/*MongoClient.connect(url, (err, client) => {
-  if (err) {
-    console.error(err)
-  
-    return
-  }
-  var dbase = client.db("persons");
-  const collection = dbase.collection('profiles')
-  //...
-})*/
 
-
-
-
+});    
+    
+      
 
 
 
     var app = express();
     var bodyParser=require('body-parser');
 
-
+    
     var port = process.env.PORT || 5000;
 
     var savedata=require("./class");
     app.set('port', port);
     app.use(express.static(__dirname + '/public'));
-
-    app.use(bodyParser.json())
+    
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -54,64 +42,62 @@ MongoClient.connect(url, { useNewUrlParser: true },function(err, db) {   //here 
       res.send(msg);
       console.log('done server')
     });
-
+    
 
     app.get('/api',(req,res)=>{
-
+     
       savedata.print();
-
+      
       res.status(200).send(savedata.jsonreturn);
        });
 
        app.get('/api/search',(req,res)=>{
-
+       
           savedata.search(req.body.age);
-
+         
           res.status(200).send('The name of these age are printed in console');
-
+           
            });
+    
 
-
-
+    
 
          app.post('/api',(req,res)=>{
-
+      
             savedata.storeData(req.body.name,req.body.age,req.body.sex,req.body.email,req.body.phonenumber,req.body.skills,req.body.study);
-
+           
             res.status(200).send(req.body.name+' '+req.body.age+' '+req.body.sex+' '+req.body.email+' '+req.body.phonenumber+' '+req.body.skills+' '+req.body.study);
+             
+             });
+
+          app.put('/api',(req,res)=>{
+             
+              savedata.editdata(req.body.index,req.body.name,req.body.age,req.body.sex,req.body.email,req.body.phonenumber,req.body.skills,req.body.study);
+           
+              res.status(200).send(req.body.name+' '+req.body.age+' '+req.body.sex+' '+req.body.email+' '+req.body.phonenumber+' '+req.body.skills+' '+req.body.study);
+               
+               
+               });
+
+
+               app.delete('/api',(req,res)=>{
+               
+                savedata.deletdata(req.body.index);
+             
+                res.status(200).send('this is profile has deleted!!!');
+                 
+                
+                 });
             
-
-          });
-
-       app.put('/api',(req,res)=>{
-
-           savedata.editdata(req.body.index,req.body.name,req.body.age,req.body.sex,req.body.email,req.body.phonenumber,req.body.skills,req.body.study);
-
-           res.status(200).send(req.body.name+' '+req.body.age+' '+req.body.sex+' '+req.body.email+' '+req.body.phonenumber+' '+req.body.skills+' '+req.body.study);
-
-
-            });
-
-
-            app.delete('/api',(req,res)=>{
-
-             savedata.deletdata(req.body.index);
-
-             res.status(200).send('this is profile has deleted!!!');
-
-
-              });
+     
 
 
 
 
+  
+  app.listen(port, function () {
 
+    console.log('Example app listening on port 3000!');
 
-
-app.listen(port, function () {
-
- console.log('Example app listening on port 3000!');
-
-});
+  });
 module.exports=app;
-                                                                                                        
