27cd320d5c16f4bb1c45484ae340183ecf11f5dc
AbdullahTaher93 <abdullahtaher@correo.ugr.es>
2019-08-25 13:35:01 +0200

    added acopio file closed #30

--- acopio.sh
+++ acopio.sh
@@ -1,17 +1,15 @@
 #! / bin / bash
 
-
-
-
-
-# We wil create a group of resources with location
-az group create --name hito4 --location francecentral
-
-# We created a security group for the network of the resource group previously created
-az network nsg create --resource-group hito4 --location francecentral --name networks
-# We will open port 80 
-az network nsg rule create --resource-group hito4 --nsg-name networks --name http80 --protocol tcp --priority 800 --destination-port-range 80 --access allow
-# We will create the virtual machine with the group of resources created previously.
-# az vm create --resource-group hito4 --name Myproject --image Canonical: UbuntuServer: 18.04-LTS: latest --admin-username abdullahtaher --admin-password mypassword --public-ip-address-allocation static - nsg networks
-# We will create the virtual machine with the group of resources created previously.
- az vm create --resource-group hito4resend --name Myproject --image credativ:Debian:9:latest --admin-username abdullahtaher --admin-password mypassword --public-ip-address-allocation static -- nsg networks
+gcloud compute instances create hito4 \
+      #To select the Ubuntu OS
+    --image-family ubuntu-1804-lts \ 
+    --image-project ubuntu-os-cloud \
+    #To select data center of europe-west2 
+    --zone europe-west2 \
+    #To select the machine type with 1 vCPU and 3.75 Memory
+    --machine-type n1-standard-1 \
+    --subnet default \
+    #To put IP static for our machine
+    --address [IP address] \
+    #To open port 80
+    --tags http-server 
